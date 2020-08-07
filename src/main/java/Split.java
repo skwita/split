1f593cf57cbad5a9211793e0068c7d9f7ce9fe71
skwita <skwita.max@gmail.com>
2020-08-07 17:40:22 +0300

    optimized function start added preprocessing and processing

--- src/main/java/Split.java
+++ src/main/java/Split.java
@@ -3,20 +3,18 @@ import org.kohsuke.args4j.*;
 import java.io.*;
 
 public class Split {
-    private BufferedReader reader;
-    private BufferedWriter writer;
-    private int countFiles;
+    private int countFiles = 1;
 
     @Option(name = "-d", usage = "defines output files names")
     private boolean isNameNum = true;
 
-    @Option(name = "-l", usage = "output files length in strings", forbids = {"-n","-c"})
+    @Option(name = "-l", usage = "output files length in strings", forbids = {"-n", "-c"})
     private int lengthInStrings = -1;
 
-    @Option(name = "-c", usage = "output files length in chars", forbids = {"-n","-l"})
+    @Option(name = "-c", usage = "output files length in chars", forbids = {"-n", "-l"})
     private int lengthInChars = -1;
 
-    @Option(name = "-n", usage = "number of output files", forbids = {"-l","-c"})
+    @Option(name = "-n", usage = "number of output files", forbids = {"-l", "-c"})
     private int numOfFiles = -1;
 
     @Option(name = "-o", usage = "default file name")
@@ -27,97 +25,81 @@ public class Split {
 
     public void start() throws IOException {
         if (defaultName.equals("-")) defaultName = inpFileName;
-        boolean isNewFile;
-        isNewFile = false;
         if (numOfFiles != -1) {
-            reader = new BufferedReader(new FileReader(inpFileName));
-            int lineNumber = 0;
+            BufferedReader reader = new BufferedReader(new FileReader(inpFileName));
             LineNumberReader lineNumberReader = new LineNumberReader(reader);
-            while (lineNumberReader.readLine() != null) lineNumber++; //counting number of lines in the inp file
+            int lineNumber = 0;
+            while (lineNumberReader.readLine() != null) lineNumber++;
             lineNumberReader.close();
-            int linesInFile = (int)Math.ceil((double)lineNumber / numOfFiles); //counting number of lines in each out file
-            for (countFiles = 0; countFiles < numOfFiles; countFiles++){
-                File file = new File (getName() + ".txt");
-                writer = new BufferedWriter(new FileWriter(file));
-                int count = 0;
-                while (count < linesInFile) {
-                    String temp = reader.readLine();
-                    if (temp != null) {
-                        writer.write(temp);
-                    } else {
-                        writer.close();
-                        reader.close();
-                        break;
-                    }
-                    count++;
-                }
-            }
+            int linesInFile = (int) Math.ceil((double) lineNumber / numOfFiles);
+            preProcess(false, linesInFile);
         } else if (lengthInChars != -1) {
-            FileInputStream reader = new FileInputStream(inpFileName);
-            while(!isNewFile){
-                File file = new File (getName() + ".txt");
-                FileOutputStream writer = new FileOutputStream(file);
-                for (int i = 0; i < lengthInChars; i++){
-                    int temp = reader.read();
-                    if (temp != -1) {
-                        writer.write(temp);
-                    } else {
-                        countFiles++;
-                        isNewFile = true;
-                        break;
-                    }
-                }
-                writer.close();
-                reader.close();
-            }
-        } else if (lengthInStrings != -1){
-            while(!isNewFile){
-                File file = new File (getName() + ".txt");
-                writer = new BufferedWriter(new FileWriter(file));
-                for (int i = 0; i < lengthInStrings; i++){
-                    String temp = reader.readLine();
-                    if (temp != null) {
-                        writer.write(temp);
-                    } else {
-                        countFiles++;
-                        isNewFile = true;
-                        break;
-                    }
-                }
-                writer.close();
-                reader.close();
-            }
+            preProcess(true, lengthInChars);
+        } else if (lengthInStrings != -1) {
+            preProcess(false, lengthInStrings);
         } else {
-            while(!isNewFile){
-                File file = new File (getName() + ".txt");
-                writer = new BufferedWriter(new FileWriter(file));
-                for (int i = 0; i < 100; i++){
-                    String temp = reader.readLine();
-                    if (temp != null) {
-                        writer.write(temp);
-                    } else {
-                        countFiles++;
-                        isNewFile = true;
-                        break;
-                    }
-                }
-                writer.close();
-                reader.close();
-            }
+            preProcess(false, 100);
         }
     }
 
-    public String getName(){
+    public String getName() {
         String result = defaultName;
         if (isNameNum) {
             result += countFiles;
         } else {
             if (countFiles <= 26) {
-                result += "a" + (char)((int)'a' + countFiles - 1);
-            } else result += (char)((int)'a' + (countFiles - 1) / 26) + + (char)((int)'a' + (countFiles - 1) % 26);
+                result += "a" + (char) ((int) 'a' + countFiles - 1);
+            } else result += (char) ((int) 'a' + (countFiles - 1) / 26) + +(char) ((int) 'a' + (countFiles - 1) % 26);
         }
         return result;
     }
 
+    public void preProcess(boolean isChar, int length) throws IOException {
+        boolean isEnd = false;
+        while (!isEnd) {
+            File file = new File(getName() + ".txt");
+            countFiles++;
+            if (isChar) {
+                FileInputStream reader = new FileInputStream(inpFileName);
+                FileOutputStream writer = new FileOutputStream(file);
+                isEnd = process(length, reader, writer);
+            } else {
+                BufferedReader reader = new BufferedReader(new FileReader(inpFileName));
+                BufferedWriter writer = new BufferedWriter(new FileWriter(file));
+                isEnd = process(length, reader, writer);
+            }
+        }
+    }
 
+    public boolean process(int length, BufferedReader reader, BufferedWriter writer) throws IOException {
+        String temp;
+        for (int i = 0; i < length; i++) {
+            temp = reader.readLine();
+            if (temp != null) {
+                writer.write(temp);
+            } else {
+                writer.close();
+                reader.close();
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public boolean process(int length, FileInputStream reader, FileOutputStream writer) throws IOException {
+        int temp;
+        for (int i = 0; i < length; i++) {
+            temp = reader.read();
+            if (temp != -1) {
+                writer.write(temp);
+            } else {
+                writer.close();
+                reader.close();
+                return true;
+            }
+        }
+        return false;
+    }
 }
+
+
