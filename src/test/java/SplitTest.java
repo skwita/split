be82f84395e83f9e4f986dd978ffeef16ca848e3
skwita <skwita.max@gmail.com>
2020-08-15 00:12:48 +0300

    tests attemp

--- src/test/java/SplitTest.java
+++ src/test/java/SplitTest.java
@@ -1,8 +1,19 @@
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SplitTest {
     @Test
-    public void testSomething() {
-
+    public void test() throws IOException {
+        File initial = new File("texts/initial");
+        Launcher.main("-d -l 4 -o - initial".split(" "));
+//        System.out.println(initial.getCanonicalPath());
+        assertEquals(new File("initial1"), new File("expected/-l/-1exp"));
+        Launcher.main("-c 150 -o First".split(" "));
+        Launcher.main("-n 3 First".split(" "));
     }
+
 }
